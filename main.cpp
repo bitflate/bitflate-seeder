f7fab49f777e27092f17098358aa63ea99780a31
Phuoc Do <dnprock@gmail.com>
2019-02-28 15:19:48 -0800

    Add bitflate testnet seed and mainnet seed hostnames

--- main.cpp
+++ main.cpp
@@ -397,11 +397,8 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
-                                       "testnet-seed.bitcoin.petertodd.org",
-                                       "testnet-seed.bluematt.me",
-                                       "testnet-seed.bitcoin.schildbach.de",
+static const string mainnet_seeds[] = {"seed.bitflate.org", ""};
+static const string testnet_seeds[] = {"testnet-seed.bitflate.org",
                                        ""};
 static const string *seeds = mainnet_seeds;
 
